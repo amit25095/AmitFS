ceb17ab7ca31ab8a776d644239d2c21e65350808
Amit Am Shalem <amit25095@gmail.com>
2022-05-04 10:08:55 -0700

    can create new files now

--- src/lib/fs.cpp
+++ src/lib/fs.cpp
@@ -4,6 +4,8 @@
 #include <afs/constants.h>
 
 #include <iostream>
+#include <sstream>
+
 #include <cstring>
 #include <cmath>
 
@@ -67,9 +69,61 @@ void FileSystem::format()
     m_disk->write(blockToAddr(1), blockSize, (const char*)dblocksTable);
     m_dblocksTable = new bool[blockSize * m_dblocksTableAmount];
     memcpy(m_dblocksTable, dblocksTable, blockSize * m_dblocksTableAmount);
-
+    
     // Create root directory
     createRootDir();
+}
+
+
+/*
+
+create a file or a directory in the file system.
+
+@param path The path to create the file/directory in.
+@param isDir Boolean which tells us if the file is directory or not.
+
+*/
+void FileSystem::createFile(const std::string& path, bool isDir)
+{
+    afsPath parsedPath = parsePath(path);
+    std::string fileName = parsedPath[parsedPath.size() - 1];
+    unsigned int fileBlock = getFreeBlock();
+
+
+    // create inode for the file.
+    inode fileInode = {
+        .flags = isDir ? (1 << DIRTYPE) : FILETYPE,
+        .fileSize = 0,
+        .firstAddr = blockToAddr(fileBlock)
+    };
+
+    reserveDBlock(fileBlock);
+    createInode(fileInode);
+
+
+    if (isDir)
+    {
+        dirSibling curr;
+        directoryData currData = { 1 };
+        strncpy(curr.name, ".", sizeof(curr.name));
+        curr.indodeTableIndex = m_header->inodes - 1;
+
+        m_disk->write(getFreeDirChunkAddr(fileInode.firstAddr), sizeof(dirSibling), (const char*)&curr);
+        m_disk->write(fileInode.firstAddr, sizeof(directoryData), (const char*)&currData);
+    }
+
+    uint32_t subdirAddr = pathToAddr(afsPath(parsedPath.begin(), parsedPath.end() - 1));
+
+    dirSibling file;
+    directoryData data;
+
+    strncpy(file.name, fileName.c_str(), sizeof(file.name));
+    file.indodeTableIndex = m_header->inodes - 1;
+
+    m_disk->write(getFreeDirChunkAddr(subdirAddr), sizeof(directorySibling), (const char*)&file);
+
+    m_disk->read(subdirAddr, sizeof(directoryData), (char*)&data);    
+    m_disk->write(subdirAddr, sizeof(directoryData), (const char*)&++data);    
 }
 
 // =========== Helpers (private functions) =========== //
@@ -93,24 +147,29 @@ void FileSystem::setHeader()
 
 /*
 
-Loads the file system if exist, if not creates the fs file and sets it super block.
+create the root directory.
 
 */
 void FileSystem::createRootDir()
 {
     unsigned int rootBlock = getFreeBlock();
+    directoryData data = { 1 };
+
+    reserveDBlock(rootBlock);
 
     inode root = {
         .flags = 1 << InodeFlags::DIRTYPE,
-        .firstAddr = rootBlock * blockSize
+        .firstAddr = rootBlock * m_disk->getBlockSize()
     };
 
     createInode(root);
+    
     dirSibling curr;
     curr.indodeTableIndex = m_header->inodes - 1;
     strncpy(curr.name, ".", sizeof(curr.name));
 
-    m_disk->write(blockToAddr(rootBlock), sizeof(dirSibling), (const char*)&curr);
+    m_disk->write(getFreeDirChunkAddr(blockToAddr(rootBlock)), sizeof(dirSibling), (const char*)&curr);
+    m_disk->write(blockToAddr(rootBlock), sizeof(uint16_t), (const char*)&data); // write the siblings amount at the beginning of directory.
 }
 
 /*
@@ -129,7 +188,7 @@ unsigned int FileSystem::getFreeBlock() const
             found = true;
     }
 
-    return found ? i : -1;
+    return found ? i - 1 : -1;
 }
 
 /*
@@ -170,7 +229,7 @@ Convert block number into address on the disk.
 @return The address of the block + the offset.
 
 */
-unsigned long FileSystem::blockToAddr(unsigned int blockNum, unsigned int offset) const
+uint32_t FileSystem::blockToAddr(unsigned int blockNum, unsigned int offset) const
 {
     return (blockNum * m_disk->getBlockSize()) + offset;
 }
@@ -186,6 +245,90 @@ Convert inode index to address in the inode table.
 */
 int FileSystem::inodeIndexToAddr(int inodeIndex) const
 {
-    return ((1 + m_dblocksTableAmount) * m_disk->getBlockSize()) * (sizeof(inode) * inodeIndex);
+    return ((1 + m_dblocksTableAmount) * m_disk->getBlockSize()) + (sizeof(inode) * inodeIndex);
 }
 
+/*
+
+Parse path into parts (seperated by /)
+
+@param pathStr the path to parse.
+
+@return vector of strings with the path splitted.
+
+*/
+afsPath FileSystem::parsePath(std::string pathStr) const
+{
+	std::stringstream ss(pathStr);
+	std::string part;
+	afsPath ans;
+
+	while (std::getline(ss, part, '/'))
+		ans.push_back(part);
+
+	return ans;
+}
+
+FileSystem::inode FileSystem::getRoot() const
+{
+    inode root;
+
+    m_disk->read(inodeIndexToAddr(0), sizeof(inode), (char*)&root);
+
+    return root;
+}
+
+FileSystem::dirSibling FileSystem::getSiblingData(const uint32_t dirAddr, int indx) const
+{
+    dirSibling sibling;
+    
+    m_disk->read((dirAddr + sizeof(directoryData)) + (indx * sizeof(dirSibling)), sizeof(dirSibling), (char*)&sibling);
+    
+    return sibling;
+}
+
+/*
+TODO: Document
+*/
+uint32_t FileSystem::pathToAddr(afsPath path) const
+{
+    inode curr;
+    directoryData data;
+    directorySibling sibling;
+
+    int currentSubDir = 1;
+    uint16_t i; 
+    int j;
+
+    curr = getRoot();
+
+    m_disk->read(curr.firstAddr, sizeof(directoryData), (char*)&data);
+
+    for(i = 1; i < path.size(); i++)
+    {
+        for (j = 0; j < data; j++)
+        {
+            sibling = getSiblingData(curr.firstAddr, j);
+            if (std::string(sibling.name) == path[currentSubDir])
+            {
+                m_disk->read(inodeIndexToAddr(sibling.indodeTableIndex), sizeof(inode), (char*)&curr);
+                break;
+            }
+        }
+        if (j == data)
+            throw std::runtime_error(std::string("No such file exist with name: ") + path[i]);
+        
+        if(!(curr.flags & (1 << DIRTYPE)))
+            throw std::runtime_error("path contains file that is not a directory.");
+    } 
+
+
+    return curr.firstAddr;
+}
+
+uint32_t FileSystem::getFreeDirChunkAddr(uint32_t dirAddr)
+{
+    directoryData data;
+    m_disk->read(dirAddr, sizeof(directoryData), (char*)&data);
+    return dirAddr + sizeof(directoryData) + (sizeof(directorySibling) * data);
+}
